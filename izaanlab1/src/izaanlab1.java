6b86bd56bbc6533a33c63c489616de56f9bf03e6
TheOriginalBabu <51098394+TheOriginalBabu@users.noreply.github.com>
2023-02-15 12:50:36 -0500

    hafdg

--- izaanlab1/src/izaanlab1.java
+++ izaanlab1/src/izaanlab1.java
@@ -18,47 +18,49 @@ public class izaanlab1 {
     public static void main(String[] args) {
      
     System.out.println("Date analyzer program - enter a date and it will be analyzed!");     
-        
-    System.out.println("Please insert the numerical integer value of the Month");        
-    String inputMonth = scan.nextLine();
-    Integer inputMonthInteger = Integer.valueOf(inputMonth); // convert to integer
     
-    if (inputMonthInteger < 1 || Integer.parseInt(inputMonth)> 12){ // range of acceptable months
+    //--------------------------        
+    
+    System.out.println("Please insert the numerical integer value of a Month");        
+    String inputMonth = scan.nextLine();
+    Integer inputMonthInteger = Integer.valueOf(inputMonth); // Convert month to integer
+    
+    if (inputMonthInteger < 1 || Integer.parseInt(inputMonth)> 12){ // Check if input is in range of acceptable months
         System.out.println("invalid month");
         System.exit(-1);
     }
-
-    //-------------------------
+    
     System.out.println("Please insert the numerical integer value of the Day");        
     String inputDay = scan.nextLine();
-    Integer inputDayInteger = Integer.valueOf(inputDay); // convert to integer
+    Integer inputDayInteger = Integer.valueOf(inputDay); // Convert day to integer
     
-    if (inputDayInteger < 1){ // Lower limit validity check for the "day" input,
+    if (inputDayInteger < 1){ // Check if input is within the LOWER limit range of acceptable days
         System.out.println("invalid day (hit lower limit)");
         System.exit(-1);
     }
     
-    //--------------------------
+    //--------------------------    
+    
+    String monthOfDate = "unknown month"; // necessary to convert month integer to string
     String seasonOfDate = "unknown season";
-    String monthOfDate = "unknown month";
-    String holidayOfDate = "unknown holiday";
+    String holidayOfDate = "unknown holiday"; 
     
     switch (inputMonthInteger) { // Switchcase to define seasons and upper limit validity check of "day" input.
-        case 1 -> { //January
+        case 1 -> { // January
             monthOfDate = "January";
             if (inputDayInteger > 31)  // Set to last day of month (define upper limit)
                 System.out.println("invalid day (hit upper limit)");
             else
                 seasonOfDate = "Winter"; // entire month is winter, so no need for day check
             }
-        case 2 -> { //Feb
+        case 2 -> { // February
             monthOfDate = "February";
             if (inputDayInteger > 29) // set to last day of month (leap year accounted for)
                 System.out.println("invalid day (hit upper limit)");
             else
                 seasonOfDate = "Winter";
             }
-        case 3 -> { //Mar
+        case 3 -> { //March
             monthOfDate = "March";
             if (inputDayInteger > 31) // set to last day of month
                 System.out.println("invalid day (hit upper limit)");
@@ -67,21 +69,21 @@ public class izaanlab1 {
             else
                 seasonOfDate = "Spring";
             }
-        case 4 -> { //Apr
+        case 4 -> { // April
             monthOfDate = "April";
             if (inputDayInteger > 30)
                 System.out.println("invalid day (hit upper limit)");
             else
                 seasonOfDate = "Spring";
             }
-        case 5 -> { //May
+        case 5 -> { // May
             monthOfDate = "May";
             if (inputDayInteger > 31)
                 System.out.println("invalid day (hit upper limit)");
             else
                 seasonOfDate = "Spring";
             }
-        case 6 -> { //June
+        case 6 -> { // June
             monthOfDate = "June";
             if (inputDayInteger > 30)
                 System.out.println("invalid day (hit upper limit)");
@@ -90,14 +92,14 @@ public class izaanlab1 {
             else
                 seasonOfDate = "Summer";            
             }
-        case 7 -> { //July
+        case 7 -> { // July
             monthOfDate = "July";
             if (inputDayInteger > 31)
                 System.out.println("invalid day (hit upper limit)");
             else
                 seasonOfDate = "Summer";              
             }
-        case 8 -> { //August
+        case 8 -> { // August
             monthOfDate = "August";
             if (inputDayInteger > 31)
                 System.out.println("invalid day (hit upper limit)");
@@ -113,14 +115,14 @@ public class izaanlab1 {
             else
                 seasonOfDate = "Fall";     
             }
-        case 10 -> { //October
+        case 10 -> { // October
             monthOfDate = "October";
             if (inputDayInteger > 31)
                 System.out.println("invalid day (hit upper limit)");
             else
                 seasonOfDate = "Fall";  
             }
-        case 11 -> { //November
+        case 11 -> { // November
             monthOfDate = "November";
             if (inputDayInteger > 31)
                 System.out.println("invalid day (hit upper limit)");
@@ -138,8 +140,8 @@ public class izaanlab1 {
             }            
     }
     
-    switch (inputMonthInteger) {
-        case 1 -> {
+    switch (inputMonthInteger) { // Determines holiday on date
+        case 1 -> { // January
             switch (inputDayInteger) {
                 case 1 -> {
                     holidayOfDate = ("New Years Day");
@@ -149,7 +151,7 @@ public class izaanlab1 {
                 }
             }
         }
-        case 2 -> {
+        case 2 -> { // February
             switch (inputDayInteger) {
                 case 4 -> {
                     holidayOfDate = ("World Cancer Day");
@@ -170,13 +172,7 @@ public class izaanlab1 {
     System.out.println("The date you entered was " + monthOfDate + " " + inputDay);
     System.out.println("The season of the date is " + seasonOfDate);
     System.out.println("This date corresponds to " + holidayOfDate);
-   
-    
-    //---------------------------------
-    
-    
-    
- 
+
     //Integer.parseInt(inputDay);
     }
 }
