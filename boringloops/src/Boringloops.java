ef18021570eb0da1a50e50d26702179d81a9ab29
TheOriginalBabu <51098394+TheOriginalBabu@users.noreply.github.com>
2023-02-22 11:59:46 -0500

    fhc

--- boringloops/src/Boringloops.java
+++ boringloops/src/Boringloops.java
@@ -1,62 +1,96 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
-
-/**
- *
- * @author Izaan Syed
- * @date Feb. 14, 2023
- * @ProjectName Date Analyzer
- * @Description First lab, takes date input then returns date info
- */
+import java.util.Scanner;
 
 public class Boringloops {
     public static void main(String[] args) {
-        /*For loops are used to count a numerically counted number of times.
-        In Java they follow this notation:
-        for(int x = 0; x < 10; x++){}
-        
-        Where: int x = 0 creates a variable to be used as a counter.
-        x < 10 is the condition that the loop will repeat until.
-        x++ is the incerment that the loop follows (ie. how much the counter changes by)
-        and the code contained in the braces {} will repeat each time.
-        */
-        
-        //To sum all of the numbers from 1 to 100 using a loop
-        int sum = 0;
-        for(int x = 0; x<=100; x++){
-            sum = sum + x;
-        }
-        System.out.println("The sum of whole numbers from 1 to 100 is: " + sum);
-        
-       //To sum all of even numbers from 1 to 100 using a loop
-        sum = 0;
-        for(int x = 0; x<=100; x+=2){
-            sum = sum + x;
-        }
-        System.out.println("The sum of all even numbers from 1 to 100 is: " + sum);
-        
-        //We can also use loops to count backwards
-        //This will display all of the numbers from 1 to 10 backwards
-        for(int x = 10; x>0; x--){
-            System.out.println(x);
-        }
-        
-        //And we can even nest for loops inside of other for loops to useful effect.
-          for(int i = 10; i>0; i--){
-              System.out.println();
-            for (int j = 0; j < i; j++){
-              System.out.print("*");
+        Scanner input = new Scanner(System.in);
+        int choice = 0;
+
+        do {
+            System.out.println("Please choose an option:");
+            System.out.println("1. Menu (WHILE LOOP)");
+            System.out.println("2. Postal Code Verification (FOR LOOP)");
+            System.out.println("3. Multiplication Table (FOR LOOP)");
+            System.out.println("4. Quit");
+            System.out.print("Enter your choice (1-4): ");
+
+            if (input.hasNextInt()) {
+                choice = input.nextInt();
+
+                switch (choice) {
+                    case 1:
+                        while (true) {
+                            System.out.println("Please select an option:");
+                            System.out.println("A. Option A");
+                            System.out.println("B. Option B");
+                            System.out.println("C. Option C");
+                            System.out.print("Enter your choice (A-C): ");
+
+                            String menuChoice = input.next().toUpperCase();
+
+                            if (menuChoice.equals("A")) {
+                                System.out.println("You selected Option A.");
+                                break;
+                            } else if (menuChoice.equals("B")) {
+                                System.out.println("You selected Option B.");
+                                break;
+                            } else if (menuChoice.equals("C")) {
+                                System.out.println("You selected Option C.");
+                                break;
+                            } else {
+                                System.out.println("Invalid option selected. Please try again.");
+                            }
+                        }
+                        break;
+
+                    case 2:
+                        while (true) {
+                            System.out.print("Please enter a postal code: ");
+                            String postalCode = input.next();
+
+                            if (postalCode.matches("[A-Z][0-9][A-Z] [0-9][A-Z][0-9]")) {
+                                System.out.println("Valid postal code entered.");
+                                break;
+                            } else {
+                                System.out.println("Invalid postal code entered. Please try again.");
+                            }
+                        }
+                        break;
+
+                    case 3:
+                        while (true) {
+                            System.out.print("Please enter an integer: ");
+
+                            if (input.hasNextInt()) {
+                                int num = input.nextInt();
+
+                                for (int i = 1; i <= 999999999; i++) {
+                                    System.out.println(num + " x " + i + " = " + (num * i));
+                                }
+
+                                break;
+                            } else {
+                                System.out.println("Invalid input entered. Please enter an integer.");
+                                input.next();
+                            }
+                        }
+                        break;
+
+                    case 4:
+                        System.out.println("Exiting program...");
+                        break;
+
+                    default:
+                        System.out.println("Invalid option selected. Please try again.");
+                        break;
+                }
+            } else {
+                System.out.println("Invalid input entered. Please enter a number.");
+                input.next();
             }
-         }
-        // Practice program
-        //Write a nested for loop that displays a calendar like format
-            // M  T  W  H  F  S  U
-            // 1  2  3  4  5  6  7
-            // 8  9  10 11 12 13 14
-            //etc...
-        
+
+            System.out.println();
+        } while (choice != 4);
+
+        input.close();
     }
 }
