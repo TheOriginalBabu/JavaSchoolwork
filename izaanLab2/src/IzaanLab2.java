0fbd00417ca3446386ec59676221dfe1c381c06c
TheOriginalBabu <51098394+TheOriginalBabu@users.noreply.github.com>
2023-03-02 12:28:37 -0500

    n

--- izaanLab2/src/IzaanLab2.java
+++ izaanLab2/src/IzaanLab2.java
@@ -15,57 +15,70 @@ import java.util.Scanner;
  */
 
 public class IzaanLab2 {
-    public static Scanner scan = new Scanner(System.in);
-    public static int intUserSelection; // program selection
-    public static String inputPostalCode;
-    public static void main(String[] args) {
-    
-    System.out.println("Enter corresponding integer (1 = Postal code, 2 = Multiplication table, 3 = Quit)");
+public static Scanner scan = new Scanner(System.in);
+public static int intUserSelection; // program selection
+public static String inputPostalCode;
+public static void main(String[] args) {
 
-    while (true) {
-        String userSelection = (scan.nextLine());
-        intUserSelection = Integer.parseInt(userSelection);
+
+while (true) {
+    System.out.println("Enter corresponding integer (1 = Postal code, 2 = Multiplication table, 3 = Quit)");
+    try{
+        intUserSelection = Integer.parseInt(scan.nextLine());
         if (intUserSelection == 1 || intUserSelection == 2 || intUserSelection == 3) break;
         System.out.println("Bad number u idiot");
-        }
+    } catch(Exception e){
+        System.out.println("Not a number u idiot");
+    }
+}
 
-    switch(intUserSelection){
-        case 1 ->{
-
-            while (true) {
-                System.out.println("postal code program");
-                String inputPostalCode = (scan.nextLine());
-                String[] inputPostalCode2 = inputPostalCode.split("");
-                System.out.println(Arrays.toString(inputPostalCode2));
-                if (inputPostalCode.length() < 6) {
-                    System.out.println("your postal code was too long");
-                    break;
-                }
-
-//string.valueof
-                try { // Checks if format of postal code is correct (A1A 1A1)
-                    Integer.parseInt(inputPostalCode2[1]);
-                    Integer.parseInt(inputPostalCode2[3]);
-                    Integer.parseInt(inputPostalCode2[5]);
-                    
-
-                    System.out.println("your postal code was " + inputPostalCode);
-                    break;
-                } catch(Exception e){
-                        System.out.println("your postal code was not in the correct format");
-                    }
+switch(intUserSelection){
+    case 1 ->{
+        while (true) {
+            System.out.println("postal code program");
+            String inputPostalCode = (scan.nextLine());
+            String[] inputPostalCode2 = inputPostalCode.split("");
+            System.out.println(Arrays.toString(inputPostalCode2));
+            if (inputPostalCode.length() > 7) {
+                System.out.println("your postal code was too long");
+                break;
             }
 
+//string.valueof
+            try { // Checks if format of postal code is correct (A1A 1A1)
+                Integer.parseInt(inputPostalCode2[1]);
+                Integer.parseInt(inputPostalCode2[4]);
+                Integer.parseInt(inputPostalCode2[6]);
+
+                System.out.println("your postal code was " + inputPostalCode);
+                break;
+            } catch(Exception e){
+                    System.out.println("your postal code was not in the correct format");
+                }
+            
         }
-        case 2 ->{
+
+    }
+    case 2 ->{
+        while (true) {
             System.out.println("multiplication program");
-            String inputMultiplication = (scan.nextLine());
-            System.out.println("you wanted to multiply " + inputMultiplication);
+            try{
+                String inputMultiplication = (scan.nextLine());
+                int number = Integer.parseInt(inputMultiplication);
+                for (int i = 1; i <= 12; i++) {
+                    System.out.println(number + " x " + i + " = " + (i * number));
+                }
+                break;
+            }catch(Exception e){
+                System.out.println("bad input u buffoon");
+                }
+            }
         }
-        case 3 ->{
-            System.out.println("u quitted");
-            System.exit(0);
-        }
+
+    case 3 ->{
+        System.out.println("u quitted");
+        System.exit(0);
     }
-    }
+}
+}
 }
