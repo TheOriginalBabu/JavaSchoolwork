0d8711473db1bbefc196b19da1591eb711b27e9b
TheOriginalBabu <51098394+TheOriginalBabu@users.noreply.github.com>
2023-03-07 12:04:53 -0500

    setup

---
+++ IzaanLab3/src/IzaanLab3.java
@@ -0,0 +1,19 @@
+/**
+ *
+ * author Izaan Syed
+ * date March. 7, 2023
+ * ProjectName IzaanLab3
+ * Description Complicated lab
+ */
+
+import java.util.Scanner;
+
+public class IzaanLab3 {
+    public static void ValidityCheck() {
+        System.out.println("Postal Code Validity Check");
+    }
+    public static void main(String[] args) {
+        System.out.println("Postal code lab 3");
+        ValidityCheck();
+    }
+}
