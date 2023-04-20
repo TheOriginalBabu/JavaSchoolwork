5c58c2b0853552bf78a0492ec6bf0a0882b8a034
TheOriginalBabu <51098394+TheOriginalBabu@users.noreply.github.com>
2023-04-20 12:26:27 -0400

    asd

---
+++ IzaanClassesLab/src/IzaanClassesLab.java
@@ -0,0 +1,12 @@
+/**
+ * author: Izaan
+ * date: 30-Mar-2022
+ * filename: IzaanClassesLab.java
+ * description: this is my lab 6 for classes, involving poker
+ */
+
+public class IzaanClassesLab {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
