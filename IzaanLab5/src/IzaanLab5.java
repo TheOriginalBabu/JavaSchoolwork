b554b2cda796886b455c780828893440fb2c40d0
TheOriginalBabu <51098394+TheOriginalBabu@users.noreply.github.com>
2023-04-26 12:51:13 -0400

    Update IzaanLab5.java

--- IzaanLab5/src/IzaanLab5.java
+++ IzaanLab5/src/IzaanLab5.java
@@ -16,9 +16,15 @@ public class IzaanLab5 {
                 return num; // returns 0 or 1
             } else {
                 try {
-                    return fibonacci(num - 1, startTime) + fibonacci(num - 2, startTime); // Recursive cal
+                    int var1 = fibonacci(num - 1, startTime);
+                    int var2 = fibonacci(num - 2, startTime); // Recursive cal
+                    if (var1 == -1 || var2 == -1) { // If either of the recursive calls return -1
+                        return -1; // returns -1
+                    } else {
+                        return var1 + var2; // returns the sum of the two recursive calls
+                    }
                 } catch (StackOverflowError e) { // Catches stack overflow error
-                    return 0; // returns 0
+                    return -1; // returns 0
                 }
             }
         } else {
@@ -30,45 +36,56 @@ public class IzaanLab5 {
 
 
     //Towers of Hanoi algorithm, found from https://www.digitalocean.com/community/tutorials/tower-of-hanoi
-    public static void hanoi(int n, char from, char to, char aux) { // n is number of disks, from is the source rod, to is the destination rod and aux is the auxiliary rod
-        if (n == 1) { // Base case
-            System.out.println("Move disk 1 from rod " + from + " to rod " + to); // Prints the move
-            return;
+
+    public static void hanoi(int n, char from, char to, char aux, long startTime) {
+        if (System.currentTimeMillis() - startTime < 10000) { // If the time elapsed is greater than 10 seconds
+            if (n == 1) { // Base case
+                System.out.println("Move disk 1 from rod " + from + " to rod " + to); // Prints the move
+                return;
+            }
+            hanoi(n - 1, from, aux, to, startTime); // Recursive call, moves n-1 disks from source to auxiliary
+            System.out.println("Move disk " + n + " from rod " + from + " to rod " + to); // Prints the move
+            hanoi(n - 1, aux, to, from, startTime); // Recursive call, moves n-1 disks from auxiliary to destination
         }
-        hanoi(n - 1, from, aux, to); // Recursive call, moves n-1 disks from source to auxiliary
-        System.out.println("Move disk " + n + " from rod " + from + " to rod " + to); // Prints the move
-        hanoi(n - 1, aux, to, from); // Recursive call, moves n-1 disks from auxiliary to destination
+        else {
+            System.out.println("The program took too long to run, exiting. Please try again with a smaller number of disks.");
+        }
     }
 
     //Quicksort algorithm, found from https://www.geeksforgeeks.org/quick-sort/
-    public static int[] quickSort(int[] arr, int low, int high) { // arr is the array to be sorted, low is the first index and high is the last index
-        if (arr == null || arr.length == 0) // Base case
-            return arr; // returns if the array is empty
-        if (low >= high) // Base case
-            return arr; // returns if the array is sorted
-        int middle = low + (high - low) / 2; // Finds the middle index
-        int pivot = arr[middle]; // Finds the pivot
-        int i = low, j = high; // Sets i to the first index and j to the last index
-        while (i <= j) { // While i is less than or equal to j
-            while (arr[i] < pivot) { // While the value at i is less than the pivot
-                i++; // Increment i
+    public static int[] quickSort(int[] arr, int low, int high, long startTime) { // arr is the array to be sorted, low is the first index and high is the last index
+        if (System.currentTimeMillis() - startTime < 10000) { // If the time elapsed is greater than 10 seconds
+            if (arr == null || arr.length == 0) // Base case
+                return arr; // returns if the array is empty
+            if (low >= high) // Base case
+                return arr; // returns if the array is sorted
+            int middle = low + (high - low) / 2; // Finds the middle index
+            int pivot = arr[middle]; // Finds the pivot
+            int i = low, j = high; // Sets i to the first index and j to the last index
+            while (i <= j) { // While i is less than or equal to j
+                while (arr[i] < pivot) { // While the value at i is less than the pivot
+                    i++; // Increment i
+                }
+                while (arr[j] > pivot) { // While the value at j is greater than the pivot
+                    j--; // Decrement j
+                }
+                if (i <= j) { // If i is less than or equal to j
+                    int temp = arr[i]; // Swap the values at i and j
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                    i++; // Increment i
+                    j--; // Decrement j
+                }
             }
-            while (arr[j] > pivot) { // While the value at j is greater than the pivot
-                j--; // Decrement j
-            }
-            if (i <= j) { // If i is less than or equal to j
-                int temp = arr[i]; // Swap the values at i and j
-                arr[i] = arr[j];
-                arr[j] = temp;
-                i++; // Increment i
-                j--; // Decrement j
-            }
+            if (low < j) // Recursive call
+                quickSort(arr, low, j, startTime);
+            if (high > i) // Recursive call
+                quickSort(arr, i, high, startTime);
+            return arr; // Returns the sorted array
         }
-        if (low < j) // Recursive call
-            quickSort(arr, low, j);
-        if (high > i) // Recursive call
-            quickSort(arr, i, high);
-        return arr; // Returns the sorted array
+        else {
+            return arr;
+        }
     }
 
     // method for user input for fibonacci
@@ -78,8 +95,12 @@ public class IzaanLab5 {
         int num = input.nextInt();
         long startTime = System.currentTimeMillis();
         int varFibonacci = fibonacci(num, startTime);
-
-        System.out.println("The fibonacci of " + num + " is " + fibonacci(num, startTime));
+        if (varFibonacci == -1) {
+            System.out.println("The program took too long to run, exiting.");
+        }
+        else {
+            System.out.println("The fibonacci of " + num + " is " + varFibonacci);
+        }
     }
 
     public static void userHanoi() {
@@ -92,7 +113,7 @@ public class IzaanLab5 {
         char to = input.next().charAt(0);
         System.out.println("Enter the auxiliary rod (the last remaining rod): ");
         char aux = input.next().charAt(0);
-        hanoi(n, from, to, aux);
+        hanoi(n, from, to, aux, System.currentTimeMillis());
     }
 
     public static void userQuicksort() {
@@ -103,7 +124,9 @@ public class IzaanLab5 {
         for (int i = 0; i < size; i++) {
             arr[i] = (int) (Math.random() * 1000); // multiplies by 1000 to get a number between 0 and 1000
         }
-        int[] sortedarr = quickSort(arr, 0, size - 1);
+        System.out.println("The randomized array is: " + java.util.Arrays.toString(arr));
+        long startTime = System.currentTimeMillis();
+        int[] sortedarr = quickSort(arr, 0, size - 1, startTime);
         System.out.println("The sorted array is: " + java.util.Arrays.toString(sortedarr));
     }
 
